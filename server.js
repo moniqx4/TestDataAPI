a7e51a3765178013791b8fc29c66c1b5b28955b6
Monica D <moniqx4@gmail.com>
2021-04-23 07:44:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,27 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/testconfigs', (req, res) => {    
+    res.status(200).json({ success: true, data: { msg: 'Shows all Test Configs' }});
+});
+
+app.get('/api/v1/testconfigs/:team', (req, res) => {    
+    res.status(200).json({ success: true, data: { msg: `Get Single Test Config ${req.params.team}`}});
+});
+
+app.post('/api/v1/testconfigs', (req, res) => {    
+    res.status(200).json({ success: true, data: { msg: 'Create New Test Config' }});
+});
+
+app.put('/api/v1/testconfigs/:team', (req, res) => {    
+    res.status(200).json({ success: true, data: { msg: `Update Test Config ${req.params.team}`}});
+});
+
+app.delete('/api/v1/testconfigs/:team', (req, res) => {    
+    res.status(200).json({ success: true, data: { msg: `Delete Test Config ${req.params.team}`}});
+});
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
